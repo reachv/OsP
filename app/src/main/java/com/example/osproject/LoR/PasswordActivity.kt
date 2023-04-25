2287f3e3d0d2d6fdb70e8f0ed04ae3b8634bf82a
reachv <reach.vann@gmail.com>
2023-04-24 20:34:31 -0700

    Post Game Initialization, Beginning of Game binding

--- app/src/main/java/com/example/osproject/LoR/PasswordActivity.kt
+++ app/src/main/java/com/example/osproject/LoR/PasswordActivity.kt
@@ -1,4 +1,4 @@
-package com.example.osproject
+package com.example.osproject.LoR
 
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
@@ -8,6 +8,7 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 import com.example.osproject.Fragment.SettingsFragment
+import com.example.osproject.R
 import com.parse.LogInCallback
 import com.parse.ParseUser
 
